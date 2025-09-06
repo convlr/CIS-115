55a7df9b2ed7ad12156507a7be43c00ccd107713
convlr <lyonsbrantley595@gmail.com>
2025-09-05 20:38:06 -0400

    some changes

--- lab2/lab2Lyons.py
+++ lab2/lab2Lyons.py
@@ -6,6 +6,7 @@
 # ========================================
 
 # Problem 1: counting the powers of 2
+
 print(2**0)
 print(2**1)
 print(2**2)
@@ -14,4 +15,19 @@ print(2**4)
 print(2**5)
 print(2**6)
 print(2**7)
+
+# Problem 2: some user int rasied to the power of 2
+
+userNumber = int(input("What power of two?: "))
+squared = userNumber ** 2
+print(f"Two to the power of {userNumber} is {squared}")
+
+# Problem 3: user int raised to user int
+
+base = int(input("What base?: "))
+exponent = int(input(f"What power of {base}?: "))
+powerResult = (base) ** exponent
+print(f"{base} to the power of {exponent} is {powerResult}")
+
+# Problem 4: convert user input to binary
 
