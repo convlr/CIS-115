b9fdfd36a9b44444124ec4c84aa258304c1454c1
convlr <lyonsbrantley595@gmail.com>
2025-09-18 17:42:38 -0400

    lab4

---
+++ lab4/lab4Lyons.py
@@ -0,0 +1,69 @@
+# ==============================================
+# NAME: Brantley Lyons
+# DATE: 9/24/2025
+# COURSE: CIS-115-NLE01
+# PURPOSE: Decision Structures and Boolean Logic
+# ==============================================
+
+# Problem 1: Practice using nested if statements
+value = int(input("Enter the number of college credits earned: "))
+if value > 90:
+        print("Senior Status")
+else:
+        if value > 60:
+                print("Junior Status")
+        else:
+                if value > 30:
+                        print("Sophmore Status")
+                else:
+                        print("Freshman status")
+
+# Problem 2: Practice using ELIF statements
+credits = int(input("Enter the number of college credits earned: "))
+if credits > 90:
+        print("Senior Status")
+
+elif credits > 60:
+        print("Junior Status")
+
+elif credits > 30:
+        print("Sophmore Status")
+
+else:
+        print("Freshman Status")
+
+# Problem 3: Practice using nested if statements
+fruits = input("Enter 'A' for apple, 'B' for banana, or 'C' for coconut: ")
+if fruits == "A" or fruits == "a":
+        print("Apple")
+else:
+        if fruits == "B" or fruits == "b":
+                print("Banana")
+        else:
+                if fruits == "C" or fruits == "c":
+                        print("Coconut")
+                else:
+                        print("Invalid Input!")
+
+# Problem 4: Practice using ELIF statements
+fruits2 = input("Enter 'A' for apple, 'B' for banana, or 'C' for coconut: ")
+if fruits2 == "A" or fruits2 == "a":
+        print("Apple")
+
+elif fruits2 == "B" or fruits2 == "b":
+        print("Banana")
+
+elif fruits2 == "C" or fruits2 == "c":
+        print("Coconut")
+
+else:
+        print("Invalid Input!")
+
+# Problem 5: Practice using if statements
+side1 = int(input("Enter the first side: ")
+side2 = int(input("Enter the second side: ")
+side3 = int(input("Enter the third side: ")
+
+
+
+       
