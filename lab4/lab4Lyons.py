516b9467696937f50de66d3c8b037d97fd63b62e
convlr <lyonsbrantley595@gmail.com>
2025-09-19 12:37:04 -0400

    lab4

--- lab4/lab4Lyons.py
+++ lab4/lab4Lyons.py
@@ -60,10 +60,35 @@ else:
         print("Invalid Input!")
 
 # Problem 5: Practice using if statements
-side1 = int(input("Enter the first side: ")
-side2 = int(input("Enter the second side: ")
-side3 = int(input("Enter the third side: ")
+side1 = float(input("Enter the first side: "))
+side2 = float(input("Enter the second side: "))
+side3 = float(input("Enter the third side: "))
 
+s1_squared = round(side1 ** 2, 0)
+s2_squared = round(side2 ** 2, 0)
+s3_squared = round(side3 ** 2, 0)
 
+is_equilateral = (side1 == side2) and (side2 == side3)
 
-       
+is_isosceles = (side1 == side2) or (side2 == side3) or (side1 == side3)
+
+is_right = (s1_squared + s2_squared == s3_squared) or \
+           (s2_squared + s3_squared == s1_squared) or \
+           (s1_squared + s3_squared == s2_squared)
+
+is_isosceles_right = is_isosceles and is_right
+
+if is_equilateral:
+    print("This is an Equilateral triangle!")
+
+elif is_isosceles_right:
+    print("This is an Isosceles Right triangle!")
+
+elif is_isosceles:
+    print("This is an Isosceles triangle!")
+
+elif is_right:
+    print("This is a Right triangle!")
+
+else:
+    print("This is a Scalene triangle!")
