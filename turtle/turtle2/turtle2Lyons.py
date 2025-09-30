10c817aff341925135ab1fe59e00994d9f0af885
convlr <lyonsbrantley595@gmail.com>
2025-09-30 17:15:12 -0400

    hello

---
+++ turtle/turtle2/turtle2Lyons.py
@@ -0,0 +1,22 @@
+import turtle
+import random
+
+turtle.color("tan")
+turtle.hideturtle()
+turtle.begin_fill()
+turtle.teleport(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.goto(-100,100)
+turtle.left(90)
+turtle.goto(-100,-100)
+turtle.left(90)
+turtle.goto(100,-100)
+turtle.end_fill()
+
+turtle.home()
+turtle.shape("turtle")
+turtle.color("green")
+turtle.forward(50)
+
+turtle.done()
