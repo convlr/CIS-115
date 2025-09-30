70ed0a64d62a62aef2d9154f27278b0bfeba550c
convlr <lyonsbrantley595@gmail.com>
2025-09-30 18:00:23 -0400

    turtle 2

--- turtle/turtle2/turtle2Lyons.py
+++ turtle/turtle2/turtle2Lyons.py
@@ -1,22 +1,46 @@
+# =================================================================
+# AUTHOR: Brantley Lyons
+# DATE: 9/30/25
+# PROGRAM: Turtle 2
+# PURPOSE: The goal of this lab is to get used to conditionals
+#		   and the random module
+# =================================================================
+
 import turtle
 import random
 
+WIDTH_BOX = 200
+HEIGHT_BOX = 200
+TURTLE_DIRECTION = random.randint(0, 359)
+TURTLE_DISTANCE = random.randint(1, 200)
+
+# Making turtles breakout box
 turtle.color("tan")
 turtle.hideturtle()
 turtle.begin_fill()
-turtle.teleport(100)
+turtle.teleport(100, -100)
 turtle.left(90)
-turtle.forward(100)
-turtle.goto(-100,100)
+turtle.forward(200)
 turtle.left(90)
-turtle.goto(-100,-100)
+turtle.forward(200)
 turtle.left(90)
-turtle.goto(100,-100)
+turtle.forward(200)
 turtle.end_fill()
 
+# Setting turtle
+turtle.showturtle()
 turtle.home()
 turtle.shape("turtle")
 turtle.color("green")
-turtle.forward(50)
+turtle.left(TURTLE_DIRECTION)
+turtle.forward(TURTLE_DISTANCE)
+
+# Check position
+x, y = turtle.position()
+
+if -100 <=x <=100 and -100 <= y <= 100:
+	print("Darn!")
+else:
+	print("Tiddles escaped!")
 
 turtle.done()
