04e38cdc822d2c99376d0efe3bb8d025145a41c3
convlr <lyonsbrantley595@gmail.com>
2025-10-11 10:25:39 -0400

    turtle3

--- turtle/turtle3/newturtle.py
+++ turtle/turtle3/newturtle.py
@@ -4,8 +4,6 @@ import random
 TURTLE_COLOR = ["red", "orange", "yellow", "green", "blue"]
 BOX_WIDTH = 200
 BOX_HEIGHT = 200
-TURTLE_DIRECTION = random.randint(0, 359)
-TURTLE_DISTANCE = random.randint(1, 100)
 
 turtle.color("tan")
 turtle.hideturtle()
@@ -15,43 +13,28 @@ turtle.pendown()
 
 turtle.begin_fill()
 for _ in range(4):
+	turtle.pencolor("brown")
 	turtle.left(90)
 	turtle.forward(BOX_WIDTH)
 turtle.end_fill()
 
+turtle.penup()
 turtle.home()
 turtle.showturtle()
 turtle.shape("turtle")
+turtle.pendown()
 
-x, y = turtle.position()
-newloop = 0
+for color in TURTLE_COLOR:
 
-while newloop < 5:
-	turtle.color(random.choice(TURTLE_COLOR))
-	turtle.setheading(TURTLE_DIRECTION)
-	turtle.forward(TURTLE_DISTANCE)
-	newloop += 1
+	turtle.color(color)
+	turtle.setheading(random.randint(0, 359))
+	turtle.forward(random.randint(1, 100))
+
+	x, y = turtle.pos()
+
 	if -100 <= x <= 100 and -100 <= y <= 100:
 		turtle.write("Darn!")
-		turtle.color(random.choice(TURTLE_COLOR))
-		turtle.setheading(TURTLE_DIRECTION)
-		turtle.forward(TURTLE_DISTANCE)
 	else:
-		turtle.write("Tiddles did not escape!")
-#for i in range(5):
-#
-#	newposition = turtle.position()
-#	turtle.color(random.choice(TURTLE_COLOR))
-#	turtle.setheading(TURTLE_DIRECTION)
-#	turtle.forward(TURTLE_DISTANCE)
-#
-#	x, y = newposition
-#
-#	if -100 <= x <= 100 and -100 <= y <= 100:
-#		turtle.write("Darn!")
-#        turtle.color(random.choice(TURTLE_COLOR))
-#        turtle.setheading(TURTLE_DIRECTION)
-#        turtle.forward(TURTLE_DISTANCE)
-#
-#	else:
+		turtle.write("Tiddles escaped!")
+
 turtle.done()
